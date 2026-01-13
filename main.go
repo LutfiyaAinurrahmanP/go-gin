81b019bab08e409f80b4a4253b696a7456fc2907
Lutfiya Ainurrahman Prasetyo <lutfiyapr.creative@gmail.com>
2026-01-13 22:23:10 +0700

    feat: configuration and learn quickstart

---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "github.com/LutfiyaAinurrahmanP/go-gin/quickstart"
+
+func main() {
+	quickstart.Example()
+	quickstart.ExampleWithHTTP()
+}
---
+++ quickstart/main.go
@@ -0,0 +1,29 @@
+package quickstart
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Example() {
+	router := gin.Default()
+	router.GET("/quickstart/example", func(ctx *gin.Context) {
+		// H = untuk shourtcut maping data
+		ctx.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	router.Run()
+}
+
+func ExampleWithHTTP()  {
+	router := gin.Default()
+
+	router.GET("/quickstart/example-with-http", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	router.Run()
+}
