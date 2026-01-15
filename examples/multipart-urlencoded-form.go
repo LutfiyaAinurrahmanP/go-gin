0178e93f41754d2d157f972d3658d10984b7d7ea
Lutfiya Ainurrahman Prasetyo <lutfiyapr.creative@gmail.com>
2026-01-15 10:21:21 +0700

    feat: Multipart/Urlencoded form

---
+++ examples/multipart-urlencoded-form.go
@@ -0,0 +1,19 @@
+package examples
+
+import "github.com/gin-gonic/gin"
+
+func MultipartUrlencodedForm() {
+	r := gin.Default()
+	r.POST("/examples/multipart-urlencoded-form/form_post", func(ctx *gin.Context) {
+		m := ctx.PostForm("message")
+		n := ctx.DefaultPostForm("nick", "anonymous")
+
+		ctx.JSON(200, gin.H{
+			"status": "posted",
+			"message": m,
+			"nick": n,
+		})
+	})
+
+	r.Run(":8080")
+}
